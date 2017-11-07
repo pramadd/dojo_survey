a35e137cbf3474eebf94f0a1942db8ac2f3b6941
Pranathi <neethu@codingdojo.com>
2017-11-06 17:14:53 -0800

    validation added

--- survey.py
+++ survey.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 
 app = Flask(__name__)
+app.secret_key = 'ThisIsSecret'
 
 @app.route('/')
 def root():
@@ -12,6 +13,17 @@ def Name():
     dojolocation = request.form['dojolocation']
     language = request.form['language']
     comment =request.form['comment']
+
+    if len(request.form['name'])<1:
+        flash("Name cannot be empty!")
+    else:
+        flash("") 
+
+    if len(request.form['comment']) >120 :
+        flash("comments should be only 120 characters")
+    else:
+        flash("")        
+
     return render_template("result.html", name = name,dojolocation=dojolocation,language=language,comment=comment)
     return redirect("/")
 
